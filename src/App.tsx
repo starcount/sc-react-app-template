6a17771c3857ec100834863439caa6b727d5d55f
Sanwar01 <sulemananwar99@gmail.com>
2022-11-10 17:01:31 +0000

    added auth0 react template files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import './styles/App.css';
+import {Route, Switch} from "react-router-dom";
+import {useAuth0} from "@auth0/auth0-react";
+import {PageLoader} from "./components/page-loader";
+import {HomePage} from "./pages/home-page";
+import {ProtectedRoute} from "./components/protected-route";
+import {ProfilePage} from "./pages/profile-page";
+import {PublicPage} from "./pages/public-page";
+import {ProtectedPage} from "./pages/protected-page";
+import {AdminPage} from "./pages/admin-page";
+import {CallbackPage} from "./pages/callback-page";
+import {NotFoundPage} from "./pages/not-found-page";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const { isLoading } = useAuth0();
+
+    if (isLoading) {
+        return (
+            <div className="page-layout">
+                <PageLoader />
+            </div>
+        );
+    }
+
+    return (
+        <Switch>
+            <Route path="/" exact component={HomePage} />
+            <ProtectedRoute path="/profile" component={ProfilePage} />
+            <Route path="/public" component={PublicPage} />
+            <ProtectedRoute path="/protected" component={ProtectedPage} />
+            <ProtectedRoute path="/admin" component={AdminPage} />
+            <Route path="/callback" component={CallbackPage} />
+            <Route path="*" component={NotFoundPage} />
+        </Switch>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, useNavigate} from "react-router-dom";
-import {Auth0Provider} from "@auth0/auth0-react";
-
+import {BrowserRouter} from "react-router-dom";
 import App from './App';
+import "./styles/styles.css";
 
-import './styles/index.css';
-import {config} from "./config";
-
-const navigate = useNavigate();
-
-const onRedirectCallback = (appState: any) => {
-    navigate(
-        appState && appState.returnTo ? appState.returnTo : window.location.pathname
-    );
-};
-
-
-const providerConfig = {
-    domain: config.authO.domain,
-    clientId: config.authO.clientId,
-    ...(config.authO.audience ? {audience: config.authO.audience} : null),
-    redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
-    onRedirectCallback,
-};
+import {AuthContainer} from "./containers";
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Auth0Provider {...providerConfig}>
-                <App/>
-            </Auth0Provider>
+            <AuthContainer>
+                <App />
+            </AuthContainer>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
