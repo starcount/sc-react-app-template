9814e8631ebe53351a86e1c19d2cc2b49eeafcb1
Sanwar01 <sulemananwar99@gmail.com>
2022-11-10 14:30:21 +0000

    added auth0 libs and files

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, useNavigate} from "react-router-dom";
+import {Auth0Provider} from "@auth0/auth0-react";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './styles/index.css';
+import {config} from "./config";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const navigate = useNavigate();
+
+const onRedirectCallback = (appState: any) => {
+    navigate(
+        appState && appState.returnTo ? appState.returnTo : window.location.pathname
+    );
+};
+
+
+const providerConfig = {
+    domain: config.authO.domain,
+    clientId: config.authO.clientId,
+    ...(config.authO.audience ? {audience: config.authO.audience} : null),
+    redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+    onRedirectCallback,
+};
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Auth0Provider {...providerConfig}>
+                <App/>
+            </Auth0Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
